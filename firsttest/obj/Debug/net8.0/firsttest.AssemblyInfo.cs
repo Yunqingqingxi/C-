8f0d571aaaa2def31678d439206fda68e713f1af
Niko <y111626395@outlook.com>
2023-12-29 16:07:31 +0800

    删除second

--- firsttest/obj/Debug/net8.0/firsttest.AssemblyInfo.cs
+++ firsttest/obj/Debug/net8.0/firsttest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("firsttest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+713e6f6978daad578eef56168d5fb57d42df232d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("firsttest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("firsttest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
