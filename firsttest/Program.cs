09304959d8badf02ed050560f9c58e0bef647f05
Yunqingqingxi <132554180+Yunqingqingxi@users.noreply.github.com>
2023-12-29 15:45:51 +0800

    添加信息

--- firsttest/Program.cs
+++ firsttest/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("我爱你");
- 
+var write = Console.ReadLine();
+Console.WriteLine($"输入的数据是:{write}");
