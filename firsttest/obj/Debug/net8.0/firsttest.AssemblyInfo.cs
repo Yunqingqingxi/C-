0e36f52a1ae2ff4fefce33456edc6fd3065323d5
Niko <y111626395@outlook.com>
2023-12-29 16:16:43 +0800

    试用

--- firsttest/obj/Debug/net8.0/firsttest.AssemblyInfo.cs
+++ firsttest/obj/Debug/net8.0/firsttest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("firsttest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b87373237c0c4d8c162620d41511bd788f345118")]
 [assembly: System.Reflection.AssemblyProductAttribute("firsttest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("firsttest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
