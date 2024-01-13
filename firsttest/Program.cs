96f2c7c6203ec3340378f4bd87c36a78ede78eb1
Yunqingqingxi <132554180+Yunqingqingxi@users.noreply.github.com>
2024-01-13 12:31:46 +0800

    合并

--- firsttest/Program.cs
+++ firsttest/Program.cs
@@ -1,5 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("我爱你");
-var write = Console.ReadLine();
-Console.WriteLine($"输入的write是:{write}");
+﻿using System;
+
+namespace f1
+{
+    class f2
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
