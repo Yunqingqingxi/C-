d210d708315a060d69e69b610e673f348d68416e
Yunqingqingxi <132554180+Yunqingqingxi@users.noreply.github.com>
2024-01-13 12:34:03 +0800

    1

--- firsttest/Program.cs
+++ firsttest/Program.cs
@@ -1,5 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("我爱你");
+<<<<<<< HEAD
+
+
+=======
 var write = Console.ReadLine();
 Console.WriteLine($"输入的是:{write}");
+>>>>>>> 7186971035b21d7841606e23559657a7cc7a0387
