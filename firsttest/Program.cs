d1b1fc51f52915790c17e43907ba0250f38e532b
Niko <y111626395@outlook.com>
2024-01-13 13:43:31 +0800

    测试

--- firsttest/Program.cs
+++ firsttest/Program.cs
@@ -6,8 +6,49 @@ namespace f1
     {
         static void Main(String[] args)
         {
-            Console.WriteLine("hello world");
+            F3.outputWord();
+
+            String temp;
+            Console.WriteLine("请输入一个数:");
+            temp = Console.ReadLine();
+            float a = float.Parse(temp);
+            Console.WriteLine("请输入一个数:");
+            temp = Console.ReadLine();
+            float b = float.Parse(temp);
+            float c =F2.相加(a,b);
+            F3.selectMax(a,b,c);
+            Console.WriteLine($"c的值是:{c}");
         }
-        
+          static float 相加(float m,float n){
+            return m+n;
+        }
+    }
+    class F3
+    {
+        public static void outputWord(){
+            Console.WriteLine("我爱中国");
+        }
+
+        public static void selectMax(float a,float b,float c){
+            float temp;
+            if(a>b)
+            {
+                temp = a;
+                a = b;
+                b = temp;
+            }
+            if(b>c)
+            {
+                temp = b;
+                b = c;
+                c = temp;
+            }
+            if(a>b){
+                temp = a;
+                a = b;
+                b = temp;
+            }
+            Console.WriteLine($"{a},{b},{c}");
+        }
     }
 }
