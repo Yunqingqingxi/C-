80f9b0c68cac51f0b81b66527170c4f18c0970ca
Niko <y111626395@outlook.com>
2024-01-14 13:37:42 +0800

    修改排序

--- firsttest/Program.cs
+++ firsttest/Program.cs
@@ -44,7 +44,7 @@ namespace f1
                 a = b;
                 b = temp;
             }
-            Console.WriteLine($"{a}>{b}>{c}");
+            Console.WriteLine($"{a}<{b}<{c}");
             float max = c;
             if(max>=17)
             {
