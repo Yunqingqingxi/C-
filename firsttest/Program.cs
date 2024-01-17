537b1651ab1e88f38e9434e61c4bf73b1cbbfb6d
Niko <y111626395@outlook.com>
2024-01-17 15:10:50 +0800

    租金查看

--- firsttest/Program.cs
+++ firsttest/Program.cs
@@ -7,16 +7,23 @@ namespace f1
         static void Main(String[] args)
         {
             String temp;
-            Console.WriteLine("请输入一个数:");
+            Console.WriteLine("请输入短期租金:");
             temp = Console.ReadLine();
             float a = float.Parse(temp);
-            Console.WriteLine("请输入一个数:");
+            Console.WriteLine("请输入长期租金:");
             temp = Console.ReadLine();
             float b = float.Parse(temp);
-            float c =F2.Sum(a,b);
-            F3.selectMax(c,a,b);
+            Console.WriteLine("请输入可接受的的差值(长租金-短租金):");
+            temp = Console.ReadLine();
+            float washPay = float.Parse(temp);
+            Console.WriteLine("请输入你的预算:");
+            temp = Console.ReadLine();
+            float washMoney = float.Parse(temp);
+            // float c =F2.Sum(a,b);
+            F3.bestPay(a,b,washPay,washMoney);
+            // F3.selectMax(c,a,b);
             // Console.WriteLine($"c的值是:{c}");
-            Console.ReadKey();
+            // Console.ReadKey();
         }
           static float Sum(float m,float n){
             return m+n;
@@ -52,6 +59,62 @@ namespace f1
             else
             {
                 Console.WriteLine("最爱萝莉了");
+            }
+        }
+        public static void bestPay(float shortPay,float longPay,float washPay,float washMoney)
+        {
+            double Pay_d;
+            double Pay_dl;
+            for(int day = 8;day<15;day++)
+            {
+                Pay_d = day * shortPay;
+                for(int deadline = 15;deadline<=100;deadline++)
+                {
+                    Pay_dl = deadline * longPay;
+                     if(Pay_d == Pay_dl && (Pay_dl-Pay_d)<washPay)
+                     {
+                        Console.WriteLine("最合适短租天数"+day+"最合适长租天数"+deadline);
+                        Console.WriteLine($"此时已经得到的Pay_d:{Pay_d}和Pay_dl:{Pay_dl}");
+                     }
+                     else
+                     {
+                        Console.WriteLine("没有相同价格或者与你的心理差价相近的租法,可以随意购买");
+                        Console.WriteLine("\n");
+                        Console.WriteLine("推荐购买:");
+                        Console.WriteLine("输入1推荐长租,输入2推荐短租");
+                        String select = Console.ReadLine();
+                        float washday;
+                        switch(select)
+                        {
+                            // 当用户用最大价格租长期时
+                            case "1":
+                            washday = washMoney/longPay;
+                            if(washday > 15 && washday <= 100)
+                            {
+                                Console.WriteLine("你可以购买的最长长租天数是"+washday);
+                            }else{
+                                if(washday < 15)
+                                Console.WriteLine("应该不可能读到这个");
+                            }
+                            break;
+                            // 当用户想用最大价格租短租时
+                            case "2":
+                            washday = washMoney/shortPay;
+                            if(washday > 0 && washday <15)
+                            {
+                                Console.WriteLine("你可以购买的最长短租天数是"+washday);
+                            }
+                            else
+                            {
+                                Console.WriteLine("你的预算充足，建议你长租");
+                                return;
+                            }
+                            break;
+                            default:
+                            break;
+                        }
+                    }
+                }
             }
         }
     }
--- firsttest/obj/Release/net8.0/firsttest.AssemblyInfo.cs
+++ firsttest/obj/Release/net8.0/firsttest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("firsttest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eede317ff0b8ff46e6ee4d51a614fd70761619c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2bf9a3b271f38e976cb7e301fa14d7d070c826a")]
 [assembly: System.Reflection.AssemblyProductAttribute("firsttest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("firsttest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
