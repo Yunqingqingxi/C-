3b4cab5ee39b5cc8b404377544ec761561be561f
Niko <y111626395@outlook.com>
2024-02-19 14:37:52 +0800

    提交修改

--- firsttest/obj/Release/net8.0/firsttest.AssemblyInfo.cs
+++ firsttest/obj/Release/net8.0/firsttest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("firsttest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+69b70b47d6fc92bafd4e77cda6df9c2714b4f4f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56a22aab2a85a6e61a247d7d7f456c0d1485ce2f")]
 [assembly: System.Reflection.AssemblyProductAttribute("firsttest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("firsttest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
